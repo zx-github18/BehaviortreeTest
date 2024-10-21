e19b11eaee9128a7a2113026a5858fab23fb18f0
zhangxu <zhangxu.alg@standard-robots.com>
2024-10-21 14:01:35 +0800

    add: 向黑板中写入和读取参数

---
+++ src/t01_first_behaviortree.cpp
@@ -0,0 +1,95 @@
+#include "behaviortree_cpp/bt_factory.h"
+
+using namespace BT;
+
+// Simple function that return a NodeStatus
+BT::NodeStatus CheckBattery()
+{
+  std::cout << "[ Battery: OK ]" << std::endl;
+  return BT::NodeStatus::SUCCESS;
+}
+
+// We want to wrap into an ActionNode the methods open() and close()
+class GripperInterface
+{
+public:
+  GripperInterface(): _open(true) {}
+    
+  NodeStatus open() 
+  {
+    _open = true;
+    std::cout << "GripperInterface::open" << std::endl;
+    return NodeStatus::SUCCESS;
+  }
+
+  NodeStatus close() 
+  {
+    std::cout << "GripperInterface::close" << std::endl;
+    _open = false;
+    return NodeStatus::SUCCESS;
+  }
+
+private:
+  bool _open; // shared information
+};
+
+// Example of custom SyncActionNode (synchronous action)
+// without ports.
+class ApproachObject : public BT::SyncActionNode
+{
+public:
+  ApproachObject(const std::string& name) :
+      BT::SyncActionNode(name, {})
+  {}
+
+  // You must override the virtual function tick()
+  BT::NodeStatus tick() override
+  {
+  std::cout << "ApproachObject: " << this->name() << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
+};
+
+int main()
+{
+    // We use the BehaviorTreeFactory to register our custom nodes
+    BehaviorTreeFactory factory;
+
+  // The recommended way to create a Node is through inheritance.
+  factory.registerNodeType<ApproachObject>("ApproachObject");
+
+  // Registering a SimpleActionNode using a function pointer.
+  // You can use C++11 lambdas or std::bind
+  factory.registerSimpleCondition("CheckBattery", [&](TreeNode&) { return CheckBattery(); });
+
+  //You can also create SimpleActionNodes using methods of a class
+  GripperInterface gripper;
+  factory.registerSimpleAction("OpenGripper", [&](TreeNode&){ return gripper.open(); } );
+  factory.registerSimpleAction("CloseGripper", [&](TreeNode&){ return gripper.close(); } );
+
+  // Trees are created at deployment-time (i.e. at run-time, but only 
+  // once at the beginning). 
+    
+  // IMPORTANT: when the object "tree" goes out of scope, all the 
+  // TreeNodes are destroyed
+  const std::string xml_dir = "/home/zx/Projects/behaviortree/xml/";
+  const std::string xml_name = "first_behaviortree_tree.xml";
+  const std::string xml_path = xml_dir + xml_name;
+  auto tree = factory.createTreeFromFile(xml_path);
+
+  // To "execute" a Tree you need to "tick" it.
+  // The tick is propagated to the children based on the logic of the tree.
+  // In this case, the entire sequence is executed, because all the children
+  // of the Sequence return SUCCESS.
+  tree.tickWhileRunning();
+
+  return 0;
+}
+
+/* Expected output:
+*
+  [ Battery: OK ]
+  GripperInterface::open
+  ApproachObject: approach_object
+  GripperInterface::close
+*/
---
+++ src/t02_blackboard_and_ports.cpp
@@ -0,0 +1,57 @@
+#include "behaviortree_cpp/action_node.h"
+#include "behaviortree_cpp/bt_factory.h"
+
+class SaySomething : public BT::SyncActionNode
+{
+    public:
+      SaySomething(const std::string &name, const BT::NodeConfig &config)
+      :BT::SyncActionNode(name, config){
+        // to do nothing
+      }
+
+      static BT::PortsList providedPorts(){
+        return {BT::InputPort<std::string>("message")};
+      }
+
+      BT::NodeStatus tick() override {
+        BT::Expected<std::string> msg = BT::TreeNode::getInput<std::string>("message");
+        if (!msg){
+            throw BT::RuntimeError("missing required input [message]: ", msg.error());
+        }
+        std::cout << "Robot says: " << msg.value() << std::endl;
+        return BT::NodeStatus::SUCCESS;
+      }
+};
+
+class ThinkWhatToSay : public BT::SyncActionNode{
+    public:
+        ThinkWhatToSay(const std::string &name, const BT::NodeConfig &config)
+        : BT::SyncActionNode(name, config) {
+            // to do nothing
+        }
+
+        static BT::PortsList providedPorts(){
+            return {BT::OutputPort<std::string>("message")};
+        }
+
+        BT::NodeStatus tick() override {
+            const char str[] = "anwser is 42";
+            BT::TreeNode::setOutput("message", str);
+            return BT::NodeStatus::SUCCESS;
+        }
+};
+
+int main(){
+    BT::BehaviorTreeFactory factory;
+
+    factory.registerNodeType<SaySomething>("SaySomething");
+    factory.registerNodeType<ThinkWhatToSay>("ThinkWhatToSay");
+
+    const std::string xml_dir = "/home/zx/Projects/behaviortree/xml/";
+    const std::string xml_name = "blackboard_and_ports_tree.xml";
+    const std::string xml_path = xml_dir + xml_name;
+    auto tree = factory.createTreeFromFile(xml_path);
+
+    tree.tickWhileRunning();
+    return 0;
+}
